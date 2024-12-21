9940bdffeb0512a8c5a5c8aeef4b183b9d436619
kaashif <kaashif@kaashif.co.uk>
2024-12-21 18:36:13 -0500

    Add exercise1 implementation

--- exercise1/deduplicate.py
+++ exercise1/deduplicate.py
@@ -1,6 +1,18 @@
 from typing import TypeVar
 
-T = TypeVar('T')
+T = TypeVar("T")
+
 
 def deduplicate_list(items: list[T]) -> list[T]:
-    return items
+    """
+    Returns list of items in item that appeared more than once,
+    in the order they first appeared.
+    """
+    item_to_count: dict[T, int] = {}
+
+    for item in items:
+        item_to_count[item] = item_to_count.get(item, 0) + 1
+
+    # Since Python 3.7, dict iteration order is insertion order,
+    # which means this gives elements in the order they first appeared.
+    return [item for item, count in item_to_count.items() if count > 1]
