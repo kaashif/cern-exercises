44268fb3ee2132f3ed009c6f56fb0cea8acca2b0
kaashif <kaashif@kaashif.co.uk>
2024-12-21 20:41:41 -0500

    Trim integer cells on put

--- src/main/java/org/cern/exercise2/SpreadsheetImpl.java
+++ src/main/java/org/cern/exercise2/SpreadsheetImpl.java
@@ -3,6 +3,16 @@ package org.cern.exercise2;
 public class SpreadsheetImpl {
     private String[][] data;
 
+    private static boolean isIntegerCell(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+            if (!Character.isDigit(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public SpreadsheetImpl(int numRows, int numColumns) {
         data = new String[numRows][numColumns];
     }
@@ -13,7 +23,11 @@ public class SpreadsheetImpl {
     }
 
     void put(int row, int column, String content) {
-        data[row][column] = content;
+        if (isIntegerCell(content)) {
+            data[row][column] = content.trim();
+        } else {
+            data[row][column] = content;
+        }
     }
 
     ValueType getValueType(int row, int column) {
