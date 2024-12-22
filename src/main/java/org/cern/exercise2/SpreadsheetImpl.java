740ff937001b74ea73ffe9b66368053ce6fc37c3
kaashif <kaashif@kaashif.co.uk>
2024-12-21 20:16:45 -0500

    Add cell storage and retrieval

--- src/main/java/org/cern/exercise2/SpreadsheetImpl.java
+++ src/main/java/org/cern/exercise2/SpreadsheetImpl.java
@@ -1,15 +1,19 @@
 package org.cern.exercise2;
 
 public class SpreadsheetImpl {
-    public SpreadsheetImpl(int row, int column) {
+    private String[][] data;
 
+    public SpreadsheetImpl(int numRows, int numColumns) {
+        data = new String[numRows][numColumns];
     }
 
     String get(int row, int column) {
-        return "";
+        var content = data[row][column];
+        return content == null ? "" : content;
     }
 
     void put(int row, int column, String content) {
+        data[row][column] = content;
     }
 
     ValueType getValueType(int row, int column) {
