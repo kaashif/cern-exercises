4fa4d5261942b8ae969a4335dc0c0c12efe75483
kaashif <kaashif@kaashif.co.uk>
2024-12-22 23:43:01 -0500

    Add failing DependencyGraph and JsonParser tests

--- src/test/java/org/cern/exercise3/DependencyGraphTest.java
+++ src/test/java/org/cern/exercise3/DependencyGraphTest.java
@@ -1,4 +1,37 @@
 package org.cern.exercise3;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RunWith(JUnit4.class)
 public class DependencyGraphTest {
+    @Test
+    public void prettyPrintsGraph() {
+        // Given
+        var dependencies = new HashMap<String, List<String>>() {{
+            put("pkg1", List.of("pkg2", "pkg3"));
+            put("pkg2", List.of("pkg3"));
+            put("pkg3", List.of());
+        }};
+
+        // When
+        var prettyPrinted = new DependencyGraph(dependencies).toString();
+
+        // Then
+        String expected = """
+                - pkg1
+                  - pkg2
+                    - pkg3
+                  - pkg3
+                - pkg2
+                  - pkg3
+                - pkg3
+                """;
+        Assertions.assertEquals(expected, prettyPrinted);
+    }
 }
