7b2ab18b818faa9cc07a4b0b6890a69a5b709ca8
kaashif <kaashif@kaashif.co.uk>
2024-12-23 00:04:14 -0500

    Add DependencyGraph pretty printer

--- src/main/java/org/cern/exercise3/DependencyGraph.java
+++ src/main/java/org/cern/exercise3/DependencyGraph.java
@@ -1,14 +1,45 @@
 package org.cern.exercise3;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
 public class DependencyGraph {
-    public DependencyGraph(Map<String, List<String>> parsedDependencies) {
+    private Map<String, List<String>> packageDependencies;
+
+    public static DependencyGraph readFile(String fileName) throws IOException {
+        return new DependencyGraph(new JsonParser(Files.readString(Path.of(fileName))).parseObject());
+    }
+
+    public static String prettyPrintFile(String fileName) throws IOException {
+        return readFile(fileName).toString();
+    }
+
+    public DependencyGraph(Map<String, List<String>> packageDependencies) {
+        this.packageDependencies = packageDependencies;
+    }
+
+    private void prettyPrint(StringBuilder stringBuilder, int indentLevel, String pkg) {
+        stringBuilder.append(" ".repeat(2 * indentLevel));
+        stringBuilder.append("- ");
+        stringBuilder.append(pkg);
+        stringBuilder.append("\n");
+
+        for (String dependency : packageDependencies.get(pkg)) {
+            prettyPrint(stringBuilder, indentLevel + 1, dependency);
+        }
     }
 
     @Override
     public String toString() {
-        return "";
+        var stringBuilder = new StringBuilder();
+
+        for (String pkg : packageDependencies.keySet()) {
+            prettyPrint(stringBuilder, 0, pkg);
+        }
+
+        return stringBuilder.toString();
     }
 }
